bf2a275616e560879978f8b388ab64604de7aa2a
Поплавський <114696320+Poplacskyi@users.noreply.github.com>
2024-11-18 00:36:03 +0200

    lab1 task2

---
+++ AQA_framework/src/main/java/lab1_2/Main.java
@@ -0,0 +1,22 @@
+package lab1_2;
+
+public class Main { // Клас з іменем Main
+    public static void main(String[] args) {
+        int[] numbers = {3, 6, 9, 12, 15, 18};
+
+        System.out.println("Elements:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+        System.out.println();
+
+        int oddCount = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 != 0) {
+                oddCount++;
+            }
+        }
+
+        System.out.println("Number of odd elements: " + oddCount);
+    }
+}
